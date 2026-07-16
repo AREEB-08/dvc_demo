752a14fd498b3b61581f5b68b942431084ebc246
AREEB-08 <mohammadareeb411@gmail.com>
2026-07-16 10:43:46 +0530

    third commit after changing data

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -8,9 +8,15 @@ except Exception as e :
     print(e.getMessage())
     
 df = df.iloc[:, 3:]
-
+ 
+# FIRST CHANGES TO SEE THE DVC 
 df = df[df['Length of Membership'] > 1]
-
 df.drop(columns=['Avg. Session Length'],inplace=True)
+
+# SECOND CHANGES TO CHECK WHETHER DVC IS WORKING PROPERLY OR NOT
+df = df[df['Time on App'] > 10]
+df.drop(columns=['Time on Website'],inplace=True)
+
+
 df.to_csv(os.path.join('data','customer.csv'))
 # print(df)
